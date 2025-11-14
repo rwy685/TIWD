8bc04af1defd2c72ea457ec8e3dcfe590fa6d060
rwy685 <rwy685@gmail.com>
2025-11-14 14:30:02 +0900

    feat.BuildObject생성

---
+++ Assets/02_Scripts/BuildSequance/BuildManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager Instance;
+
+    private List<IBuildable> buildables = new List<IBuildable>();
+    public float interactDistance = 5f;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Register(IBuildable buildObject)
+    {
+        buildables.Add(buildObject);
+    }
+}
---
+++ Assets/02_Scripts/BuildSequance/BuildObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public interface IBuildable
+{
+    int NeededResource { get; }
+    int CurrentResource { get; }
+    void AddResource(int amount);
+    bool IsComplete { get; }
+    void Build();
+}
+
+public abstract class BuildObject : MonoBehaviour, IBuildable
+{
+    public int NeededResource { get; protected set; }
+    public int CurrentResource { get; protected set; }
+    public bool IsComplete
+    {
+        get
+        {
+            return CurrentResource >= NeededResource;
+        }
+    }
+
+    public event Action OnComplete;
+
+    public void AddResource(int amount)
+    {
+        CurrentResource += amount;
+        if (CurrentResource >= NeededResource)
+        {
+            CurrentResource = NeededResource;
+            OnComplete?.Invoke();
+        }
+    }
+    public abstract void Build();
+}
