19fa1c942aba31dda875ce54ed0731e3a059ddf4
arara0250 <arara4281@gmail.com>
2025-11-17 11:19:41 +0900

    refactor : ItemType enum 추출

--- Assets/05_Data/Item/ItemData.cs
+++ Assets/05_Data/Item/ItemData.cs
@@ -2,13 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemType
-{
-    Equipable,      // 장착 아이템
-    Consumable,     // 소비 아이템
-    Resource        // 기타 자원
-}
-
 [CreateAssetMenu(fileName = "NewItem", menuName = "New Item")]
 public class ItemData : ScriptableObject
 {
