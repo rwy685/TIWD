8bad5265eca85738a6c403c940c1f8a86c91ff6e
rwy685 <rwy685@gmail.com>
2025-11-18 20:00:22 +0900

    fix.게임매니저 추후 수정

--- Assets/02_Scripts/Managers/GameManager.cs
+++ Assets/02_Scripts/Managers/GameManager.cs
@@ -15,7 +15,7 @@ public class GameManager : MonoBehaviour
     public SpawnManager spawnManager;
     public CharacterManager characterManager;
     public BuildManager buildManager; // 빌드매니저 추가함
-    public BuildModeManager buildModeManager; // 건축모드매니저 추가
+  
 
     public float curTime = 0f;
     public float nightTime;
