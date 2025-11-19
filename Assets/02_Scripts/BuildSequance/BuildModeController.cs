a664e2f2296106e503027e53dcb431a20b3f21f4
rwy685 <rwy685@gmail.com>
2025-11-19 14:48:09 +0900

    feat.BuildModeManager의 catalog기반 생성

--- Assets/02_Scripts/BuildSequance/BuildModeController.cs
+++ Assets/02_Scripts/BuildSequance/BuildModeController.cs
@@ -6,23 +6,29 @@ using UnityEngine.InputSystem;
 public class BuildModeController : MonoBehaviour
 {
     private BuildModeManager buildMode;
-    public BuildData testBuildData; // Inspector에서 테스트용 SO 넣어두기
 
-    private void Start()
+
+    private void Awake()
     {
         buildMode = GameManager.Instance.buildModeManager;
     }
 
-    private void Update()
+    //
+    // 빌드모드 진입 입력 (B키로 예정)
+    //
+    public void OnBuildModeToggle(InputAction.CallbackContext ctx)
     {
-        if (buildMode == null) return;  // 안전장치
+        if (!ctx.started) return;
 
-        if (Keyboard.current.digit1Key?.wasPressedThisFrame == true)
+        if (!buildMode.IsBuildingMode)
         {
-            if (testBuildData != null)
-                buildMode.EnterBuildMode(testBuildData);
-            else
-                Debug.LogWarning("testBuildData가 설정되지 않았습니다!");
+            Debug.Log("Build Mode Enter");
+            buildMode.EnterBuildMode();      // 빌드모드 진입
+        }
+        else
+        {
+            Debug.Log("Build Mode Exit");
+            buildMode.ExitBuildMode(); // 빌드모드 종료
         }
     }
 
@@ -34,32 +40,31 @@ public class BuildModeController : MonoBehaviour
     // 마우스 좌클릭: 해당 위치 설치
     public void OnBuildPlace(InputAction.CallbackContext ctx)
     {
+        if (!ctx.started) return;
         if (!buildMode.IsBuildingMode) return;
-        if (ctx.phase == InputActionPhase.Started)
-            buildMode.TryPlaceStructure();
+
+        buildMode.TryPlaceStructure();
     }
 
     // Q: 사전구조물 설치방향 왼쪽으로 돌리기(반시계방향)
     public void OnBuildRotateLeft(InputAction.CallbackContext ctx)
     {
-        if (!buildMode.IsBuildingMode) return;
-        if (ctx.phase == InputActionPhase.Started)
+        if (ctx.started && buildMode.IsBuildingMode)
             buildMode.RotateLeft();
     }
+
 
     // E: 사전구조물 설치방향 오른쪽으로 돌리기(시계방향)
     public void OnBuildRotateRight(InputAction.CallbackContext ctx)
     {
-        if (!buildMode.IsBuildingMode) return;
-        if (ctx.phase == InputActionPhase.Started)
+        if (ctx.started && buildMode.IsBuildingMode)
             buildMode.RotateRight();
     }
 
     // ESC: 설치 취소
     public void OnBuildCancel(InputAction.CallbackContext ctx)
     {
-        if (!buildMode.IsBuildingMode) return;
-        if (ctx.phase == InputActionPhase.Started)
+        if (ctx.started && buildMode.IsBuildingMode)
             buildMode.ExitBuildMode();
     }
 }
