d846e4dc11bfadd6e066b7fc4e9405049e0b7078
adelipingu14 <adelipingu14@gmail.com>
2025-11-19 18:28:20 +0900

    refector: AudioManager 리펙토링 테스트

--- Assets/02_Scripts/Managers/AudioManager.cs
+++ Assets/02_Scripts/Managers/AudioManager.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
 
+    AudioSource audioSource;
+    public enum BGMType
+    { 
+        Day,
+        Night,
+        GameOver,
+    }
+
+    public Dictionary<BGMType, AudioClip> bgms;
+
+    public void PlayBGM(BGMType type)
+    {
+        audioSource.clip = bgms[type];
+        audioSource.Play();
+    }
 }
 
