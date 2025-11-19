fad4b6e35bf617a82a9091ba45511afeb6ce038d
jaewoo0530 <6672jjw@gmail.com>
2025-11-19 10:11:47 +0900

    feat: 장착아이템 사용

--- Assets/02_Scripts/Entities/Player/Equipment.cs
+++ Assets/02_Scripts/Entities/Player/Equipment.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class Equipment : MonoBehaviour
 {
     public Equip curEquip;
+    public Transform equipParent;
     private Player player;
+    private PlayerController controller;
 
     void Start()
     {
         player = GameManager.Instance.characterManager.player;
+        controller = player.controller;
     }
 
     public void EquipNew(ItemData data)
     {
         UnEquip();
-       // curEquip = Instantiate(data.equipPrefab, equipParent).GetComponent<Equip>();
+        curEquip = Instantiate(data.equipPrefab, equipParent).GetComponent<Equip>();
         Debug.Log("장착");
     }
 
@@ -29,6 +33,14 @@ public class Equipment : MonoBehaviour
         {
             Destroy(curEquip.gameObject);
             curEquip = null;
+        }
+    }
+
+    public void OnUseEquipItem(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && curEquip != null && controller.canLook)
+        {
+            curEquip.OnUse();
         }
     }
 }
--- Assets/02_Scripts/Item/Equip.cs
+++ Assets/02_Scripts/Item/Equip.cs
@@ -4,4 +4,8 @@ using UnityEngine;
 
 public class Equip : MonoBehaviour
 {
+    public virtual void OnUse()
+    {
+
+    }
 }
