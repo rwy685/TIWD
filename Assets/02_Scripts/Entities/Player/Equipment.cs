1959b47c4cb3f2db25a466e2d0d578b42082cb22
jaewoo0530 <6672jjw@gmail.com>
2025-11-19 11:03:40 +0900

    update: 레이어 및 프리팹 변경

--- Assets/02_Scripts/Entities/Player/Equipment.cs
+++ Assets/02_Scripts/Entities/Player/Equipment.cs
@@ -1,11 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XR;
 
 public class Equipment : MonoBehaviour
 {
