7836588002309ac7154be845508db329ea315f0e
jaewoo0530 <6672jjw@gmail.com>
2025-11-20 23:29:07 +0900

    feat: item 사용로직

--- Assets/02_Scripts/Item/ItemObject.cs
+++ Assets/02_Scripts/Item/ItemObject.cs
@@ -16,4 +16,26 @@ public class ItemObject : MonoBehaviour, IInteractable
         GameManager.Instance.characterManager.player.addItem?.Invoke();
         Destroy(gameObject);
     }
+
+    public void Use()
+    {
+        if (itemData.recoveryData != null)
+        {
+            foreach (var type in itemData.recoveryData)
+            {
+                if (type.consumableType == ConsumableType.Health)
+                {
+                    GameManager.Instance.characterManager.player.condition.Heal(type.recoveryAmount);
+                }
+                if (type.consumableType == ConsumableType.Hunger)
+                {
+                    GameManager.Instance.characterManager.player.condition.Drink(type.recoveryAmount);
+                }
+                if (type.consumableType == ConsumableType.Thirst)
+                {
+                    GameManager.Instance.characterManager.player.condition.Eat(type.recoveryAmount);
+                }
+            }
+        }
+    }
 }
